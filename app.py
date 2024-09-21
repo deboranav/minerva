be48bd2ae3efd7c3fc3a72ee8a6d77d96a9f89d5
deboranav <debnoemy@gmail.com>
2024-09-21 00:05:29 -0300

    mapa 3

--- app.py
+++ app.py
@@ -343,7 +343,9 @@ st.components.v1.html(open('mapa.html', 'r').read(), height=600)
 
 st.title('Distribuição de votos em candidatos por bairro')
 
-df_grouped_bairros_cand = df.groupby(['BAIRRO', 'NM_VOTAVEL'])['QT_VOTOS'].sum().reset_index()
+df_grouped_bairros_cand = df.groupby(['BAIRRO', 'NM_VOTAVEL', 'LEGENDA'])['QT_VOTOS'].sum().reset_index()
+df_grouped_bairros_cand = df_grouped_bairros_cand[df_grouped_bairros_cand['LEGENDA'] == 0]
+
 candidatos = df_grouped_bairros_cand['NM_VOTAVEL'].unique()
 candidato_selecionado = st.selectbox('Selecione o Candidato', candidatos)
 
