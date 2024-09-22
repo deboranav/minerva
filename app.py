fbb67401519a62aea418534eeb9e6533ac008ae5
deboranav <debnoemy@gmail.com>
2024-09-22 14:30:09 -0300

    Graf 0

--- app.py
+++ app.py
@@ -121,8 +121,8 @@ if cargo == 'VEREADOR':
 
 if cargo == 'PREFEITO':
     df_top4_pref = df.groupby(['NM_VOTAVEL','SG_PARTIDO'])['QT_VOTOS'].sum().reset_index()
-    df_top4_pref = df_top4_pref[df_top4_pref['NM_VOTAVEL'] != 'VOTO NULO']
-    df_top4_pref = df_top4_pref[df_top4_pref['NM_VOTAVEL'] != 'VOTO BRANCO']
+    #df_top4_pref = df_top4_pref[df_top4_pref['NM_VOTAVEL'] != 'VOTO NULO']
+    #df_top4_pref = df_top4_pref[df_top4_pref['NM_VOTAVEL'] != 'VOTO BRANCO']
 
     df_top4_pref = df_top4_pref.sort_values(by='QT_VOTOS', ascending=False).head(4)
 
