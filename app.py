0284d2bc527a01ffb8aa9fb5f295c83d4eee10a5
deboranav <debnoemy@gmail.com>
2024-09-23 02:59:25 -0300

    Graf 0

--- app.py
+++ app.py
@@ -100,7 +100,7 @@ st.set_page_config(layout="wide")
 st.sidebar.markdown("""
     <style>
     .sidebar-container {
-        background-color: #181818;
+        background-color: #121212;
         padding: 15px;
         margin-bottom: 15px;
         border-radius: 10px;
